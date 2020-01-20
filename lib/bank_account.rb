8ffe77fc44b846182b8251ec9f8ba24f30572df5
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-20 16:52:52 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,6 +1,6 @@
 class BankAccount
   attr_accessor :balance, :status
-  attr_reader :name 
+  attr_reader :name
   # @@account = []
 
   def initialize(name = "Bob", balance = 1000, status = 'open')
@@ -11,9 +11,8 @@ class BankAccount
 
   end
 
-  def deposit
-    @@account << deposit
-
+  def deposit(deposit_amount)
+    self.balance += deposit_amount
   end
 
 end
