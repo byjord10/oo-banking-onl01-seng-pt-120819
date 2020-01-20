53baa2ccf404766e416e8c9d4ff60a974f2862c3
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-20 17:22:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -9,4 +9,29 @@ class Transfer
     @amount = amount
   end
 
+  def valid?
+    if valid? && sender.balance > amount && self.status == "pending"
+      sendr.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      reject_transfer
+    end
+  end
+  def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      receiver.balance -= amount
+      sender.balance += amount
+      self.status = "reversed"
+    else
+      reject_transfer
+    end
+  end
+
+  def reject_transfer
+    self.status = "rejected"
+    "Transaction rejected. Please check your account balance."
+  end
+end
+
 end
