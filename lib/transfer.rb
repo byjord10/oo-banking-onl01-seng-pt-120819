647d6341e96f0d0ea6fece39169f80654f0272c1
Brianna Jordan <bryvjordan9@gmail.com>
2020-01-20 17:12:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
-  attr_accessor :transfer, :sender, :receiver, :status
+  attr_accessor :transfer, :sender, :receiver
+  attr_reader :status
 
   def initialize(transfer, sender, receiver, status = 'pending')
     @transfer = transfer
-    @sender  = sender
+    @sender = sender
     @receiver = receiver
     @status = status
-  end 
+  end
 
 end
